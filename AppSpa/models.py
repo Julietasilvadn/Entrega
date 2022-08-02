6a48f48b442657451461e26be641ff697fc85a4d
Juan Echenique <echejuan003@gnmail.com>
2022-08-02 07:01:14 -0300

    creacion de models

--- AppSpa/models.py
+++ AppSpa/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+
+    nombre=models.CharField(max_length=40)
+    apellido= models.CharField(max_length=30)
+    email= models.EmailField()
+
+
+class Mascota(models.Model):
+    nombre= models.CharField(max_length=30)
+    raza= models.CharField(max_length=30)
+    edad= models.IntegerField()
+
+
