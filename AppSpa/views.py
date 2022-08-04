88deb70e1191df12d8e533f2b09565ef7b8aa449
Julieta Silva <juuli.siilva@gmail.com>
2022-08-04 12:55:49 -0300

    arreglo de bootstrap

--- AppSpa/views.py
+++ AppSpa/views.py
@@ -1,39 +1,12 @@
 from django.shortcuts import render
+from urllib import request
+from urllib.request import Request
 from django.http import HttpResponse
 from datetime import date, datetime 
-from AppSpa.forms import Cliente, Mascota
 from AppSpa.models import Usuario, Mascota
+from django.urls import reverse, reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-
-def inscripcion (request):
-    if request.method == 'POST':
-        inscripcion_cliente = Cliente (request.POST)
-        if inscripcion_cliente.is_valid:
-            datos = inscripcion_cliente.cleaned_data
-            cliente = Usuario (nombre=datos['nombre'], apellido=datos['apellido'], dni=datos['dni'], email=datos['email'], contraseña=datos["contraseña"], creado=datos['fecha de creacion'], actualizado=datos['fecha de cambio '])
-            cliente.save()
-
-            return render (request, 'index.html', {'mensaje':'Cliente ingresado con exito'})
-
-        else:
-            inscripcion_cliente = Cliente ()
-
-        return render(request, 'index.html', {'incrispcion_cliente': inscripcion_cliente})
-
-
-def animal (request):
-    if request.method == 'POST':
-        lista_mascota = Mascota (request.POST)
-        if lista_mascota.is_valid:
-                datos = lista_mascota.cleaned_data
-                mascota = Mascota (nombre=datos['nombre'], raza=datos['raza'], edad=datos['edad'])         
-                mascota.save()
-
-                return render (request, 'index.html', {'mensaje':'la mascota ha sido ingresada'})
-        else:
-                lista_mascota = Mascota ()
-
-        return render(request, 'index.html', {'lista_mascota': lista_mascota})
 
 def mostrar_login(request):
     return render(request, "AppSpa/login.html")
@@ -49,3 +22,5 @@ def mostrar_perfil(request):
 
 def mostrar_reserva(request):
     return render(request, "AppSpa/reserva.html")
+
+
